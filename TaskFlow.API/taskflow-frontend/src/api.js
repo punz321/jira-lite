7eb766529d65530e4eaa36ef87b3f09e63a80fc8
Pankhuri <thakurpankhuri803@gmail.com>
2025-10-14 21:58:22 +0530

    adding componenets for fetching and adding tasks

--- TaskFlow.API/taskflow-frontend/src/api.js
+++ TaskFlow.API/taskflow-frontend/src/api.js
@@ -1,5 +1,6 @@
 const API_BASE = "http://localhost:5104/api";
 
+//component for fetching and adding books
 export async function getBooks() {
   const res = await fetch(`${API_BASE}/books`);
   return res.json();
@@ -10,6 +11,21 @@ export async function addBook(book) {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(book),
+  });
+  return res.json();
+}
+
+//component for fetching and adding tasks
+export async function getTasks() {
+  const res = await fetch(`${API_BASE}/tasks`);
+  return res.json();
+}
+
+export async function addTask(task) {
+  const res = await fetch(`${API_BASE}/tasks`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(task),
   });
   return res.json();
 }
