20648ba87b4015f6b9e9e3ac4e0b195d0210cb3f
Pankhuri <thakurpankhuri803@gmail.com>
2025-10-16 15:16:40 +0530

    registered User.cs in AppDbContext

--- TaskFlow.API/Data/AppDbContext.cs
+++ TaskFlow.API/Data/AppDbContext.cs
@@ -6,12 +6,10 @@ namespace TaskFlow.API.Data
     public class AppDbContext : DbContext
      {
         public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
-
+            : base(options) {}
         public DbSet<Book> Books { get; set; }
         public DbSet<TaskItem> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
 
     }
 }
